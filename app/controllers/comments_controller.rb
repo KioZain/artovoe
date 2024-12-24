e1b2c4f79441ade20464c1669841bbbb858c4a8a
maxim_kio <enderbroxa@yandex.ru>
2024-12-24 16:45:42 +0300

    turbo-comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,53 @@
 class CommentsController < ApplicationController
-  load_and_authorize_resource only: [ :create, :destroy ]
+  load_and_authorize_resource only: [ :create, :destroy, :edit, :update ]
   before_action :set_post
 
-def index
+ def index
     @comments = @post.comments
   end
 
-  def create
+ def create
     @comment = @post.comments.new(comment_params)
-    @comment.user = current_user # или другой механизм определения пользователя
+    @comment.user = current_user
 
-    if @comment.save
-      redirect_to post_path(@post)
-    else
-      redirect_to post_path(@post)
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream
+        format.html { redirect_to post_path(@post) }
+      else
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.update("comment_form",
+            partial: "comments/form",
+            locals: { post: @post })
+        }
+        format.html { redirect_to post_path(@post) }
+      end
     end
   end
 
-  def destroy
-    @post = Post.find(params[:post_id])
+  def edit
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @comment = @post.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.turbo_stream
+      else
+        format.turbo_stream { render :edit }
+      end
+    end
+  end
+
+def destroy
     @comment = @post.comments.find(params[:id])
     @comment.destroy
-    redirect_to post_path(@post)
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@comment) }
+      format.html { redirect_to post_path(@post) }
+    end
   end
 
   private
